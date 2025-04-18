86403b2f89b25f97229ee433adaf0e0e56887cc8
Hannes <hannes-dev@mailbox.org>
2025-04-18 23:16:35 +0200

    server for printing

---
+++ print_server/src/main.rs
@@ -0,0 +1,74 @@
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::post;
+use axum::{Json, Router};
+use escpos::driver::*;
+use escpos::errors::PrinterError;
+use escpos::printer::Printer;
+use escpos::printer_options::PrinterOptions;
+use escpos::utils::*;
+
+use chrono::{self, DateTime, Local};
+use serde::Deserialize;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/fax", post(fax_message));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn fax_message(Json(fax): Json<FaxMessage>) -> Result<impl IntoResponse, Error> {
+    dbg!(&fax);
+    print_message(fax)?;
+    Ok(StatusCode::OK)
+}
+
+fn print_message(fax: FaxMessage) -> Result<(), PrinterError> {
+    let driver = NativeUsbDriver::open(0x04b8, 0x0e28).unwrap();
+    Printer::new(driver, Protocol::default(), Some(PrinterOptions::default()))
+        .init()?
+        .debug_mode(Some(DebugMode::Dec))
+        .justify(JustifyMode::LEFT)?
+        .smoothing(true)?
+        .writeln(&format!("{}", fax.time.format("%F %R")))?
+        .feed()?
+        .writeln(&fax.message)?
+        .feed()?
+        .writeln(&format!("{} - {}", fax.from, fax.ip))?
+        .print_cut()?;
+
+    Ok(())
+}
+
+enum Error {
+    PrinterError(PrinterError),
+    Other(String),
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> axum::response::Response {
+        let msg = match self {
+            Error::PrinterError(e) => format!("Printer Error: {}", e),
+            Error::Other(e) => e,
+        };
+
+        (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response()
+    }
+}
+
+impl From<PrinterError> for Error {
+    fn from(e: PrinterError) -> Self {
+        Error::PrinterError(e)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct FaxMessage {
+    time: DateTime<Local>,
+    message: String,
+    from: String,
+    ip: String,
+}
