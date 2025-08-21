1b562e4a830070c279cd4aa8371f33359092575f
Hannes <hannes-dev@mailbox.org>
2025-08-21 23:46:10 +0200

    refactor print commands

--- print_server/src/main.rs
+++ print_server/src/main.rs
@@ -14,33 +14,14 @@ use types::FaxMessage;
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/fax", post(fax_message));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn fax_message(Json(fax): Json<FaxMessage>) -> Result<impl IntoResponse, Error> {
     dbg!(&fax);
-    print_message(fax)?;
+    fax.print()?;
     Ok(StatusCode::OK)
-}
-
-fn print_message(fax: FaxMessage) -> Result<(), PrinterError> {
-    let driver = NativeUsbDriver::open(0x04b8, 0x0e28).unwrap();
-    Printer::new(driver, Protocol::default(), Some(PrinterOptions::default()))
-        .init()?
-        .debug_mode(Some(DebugMode::Dec))
-        .justify(JustifyMode::LEFT)?
-        .smoothing(true)?
-        .writeln(&format!("{}", fax.time.format("%F %R")))?
-        .feed()?
-        .writeln(&fax.message)?
-        .feed()?
-        .writeln(&format!("{} - {}", fax.from, fax.ip))?
-        .print_cut()?;
-
-    Ok(())
 }
 
 enum Error {
@@ -62,5 +43,28 @@ impl IntoResponse for Error {
 impl From<PrinterError> for Error {
     fn from(e: PrinterError) -> Self {
         Error::PrinterError(e)
+    }
+}
+
+trait Printable {
+    fn print(&self) -> Result<(), PrinterError>;
+}
+
+impl Printable for FaxMessage {
+    fn print(&self) -> Result<(), PrinterError> {
+        let driver = NativeUsbDriver::open(0x04b8, 0x0e28).unwrap();
+        Printer::new(driver, Protocol::default(), Some(PrinterOptions::default()))
+            .init()?
+            .debug_mode(Some(DebugMode::Dec))
+            .justify(JustifyMode::LEFT)?
+            .smoothing(true)?
+            .writeln(&format!("{}", self.time.format("%F %R")))?
+            .feed()?
+            .writeln(&self.message)?
+            .feed()?
+            .writeln(&format!("{} - {}", self.from, self.ip))?
+            .print_cut()?;
+
+        Ok(())
     }
 }
